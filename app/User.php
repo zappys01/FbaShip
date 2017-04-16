0a14b2c2cec8c9db14f63e3b997e54321f84e29c
zappys@github.com <alexandru.berghian@gmail.com>
2017-04-16 23:58:46 +0300

    added IDE_HELPER

--- app/User.php
+++ app/User.php
@@ -5,12 +5,19 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
+/**
+ * App\User
+ *
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are mass assignable hello world.
      *
      * @var array
      */
